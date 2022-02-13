e54db681a5ad6fa63cf798376cecdf46a465dd82
javier <rampxxxx@gmail.com>
2022-02-13 12:34:17 +0100

    ownership, borrow, (mut) references(II).
    - Several references which are not allowed, except if are all
      inmutables.
    - Cannot dangle a pointer.
    
    Signed-off-by: javier <rampxxxx@gmail.com>

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -39,6 +39,31 @@ fn main() {
     let mut s6 = String::from("hello s6");
     println!("borrows ... {}", calculate_length(&s6));
     println!("mutable reference ... {}", calculate_length_mut(&mut s6));
+    //
+    //
+
+    let mut counter = 0;
+    let mut s7 = String::from("hello s7");
+    println!("crete s7 ... {}", s7);
+    loop {
+        if counter < 100 {
+            // When some mem has no ownership var ...is deleted?
+            s7 = String::from("hello s7 2");
+            println!("crete s7 ... and again {}", s7);
+            counter += 1;
+        } else {
+            break;
+        }
+    }
+    // Two (in)mutable references ... Error
+    let mut s8 = String::from("hello s8");
+    let r1 = &mut s8;
+    //let r2 = &mut s8;
+    //let r2 = &s8;
+    println!("Double borrow s8 ... {} ", r1);
+    // But after first reference last used, you can!!!
+    let r2 = &s8;
+    println!("Second reference borrow s8 ... {}", r2);
 }
 
 fn get_scalar(a: u32) {
@@ -61,3 +86,9 @@ fn calculate_length_mut(s: &mut String) -> usize {
     s.push_str("add extra");
     s.len()
 }
+/*
+fn dangle() -> &'static String {
+    let s = String::from("dangle pointer");
+    &s
+}
+*/
